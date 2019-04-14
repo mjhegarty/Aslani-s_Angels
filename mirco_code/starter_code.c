669c30b4ac9bbcf8a62bfb2ae969d0a3508f6b64
willhaftel <willhaftel@gwu.edu>
2019-04-14 16:20:49 -0400

    Update starter_code.c
    now supports analog values through the pin correctly

--- mirco_code/starter_code.c
+++ mirco_code/starter_code.c
@@ -3,24 +3,21 @@
 //Wiring: GND of HC-05 to GND Arduino, VCC of HC-05 to VCC Arduino, TX HC-05 to Arduino Pin 10 (RX) RX HC-05 to Arduino Pin 11 (TX) 
 SoftwareSerial bt (1, 0); // RX | TX
 
-pinMode(A3, INPUT);
-int sensorPin = A3;
+int sensorPin = A0;
 
-//int sensorValue = 0;
-int btdata;
+int sensorValue = 0;
 
 void setup() {
   bt.begin(9600);           //  setup serial
+  pinMode(A0, INPUT);
 }
 
 void loop() {
-    btdata = analogRead(sensorPin);  // read the input pin
+    sensorValue = analogRead(sensorPin);  // read the input pin
     //values need to be seperated by commas and end in semicolon
 
-    bt.print(String(btdata) + " was sent");
+    bt.print(String(sensorValue) + " was sent"); //message to the receiving device
     bt.print("\n");
-    //digitalWrite(";");
-    //bt.println ("worked!\n");
-    //message to the receiving device
-    delay(20);//delay is related to the sampling rate
+    
+    delay(1000);//delay is related to the sampling rate
 }
