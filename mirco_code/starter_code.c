bacb37e95caad4c202266b8c6a59558c05a2b663
willhaftel <willhaftel@gwu.edu>
2019-04-18 11:19:49 -0400

    Update starter_code.c
    now supports multiplexed inputs

--- mirco_code/starter_code.c
+++ mirco_code/starter_code.c
@@ -2,22 +2,29 @@
 //Default password is 1234 
 //Wiring: GND of HC-05 to GND Arduino, VCC of HC-05 to VCC Arduino, TX HC-05 to Arduino Pin 10 (RX) RX HC-05 to Arduino Pin 11 (TX) 
 SoftwareSerial bt (1, 0); // RX | TX
+int sensorPin0 = A0;
+int sensorPin1 = A1;
 
-int sensorPin = A0;
-
-int sensorValue = 0;
+int sensorValue0 = 0;
+int sensorValue1 = 0;
+//int btdata;
 
 void setup() {
   bt.begin(9600);           //  setup serial
   pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
 }
 
 void loop() {
-    sensorValue = analogRead(sensorPin);  // read the input pin
+    sensorValue0 = analogRead(sensorPin0);  // read the input pin
+    sensorValue0 = analogRead(sensorPin0);
     //values need to be seperated by commas and end in semicolon
-
-    bt.print(String(sensorValue) + " was sent"); //message to the receiving device
+    sensorValue1 = analogRead(sensorPin1);
+    sensorValue1 = analogRead(sensorPin1);
+    bt.print(String(sensorValue0) + " was sent from 0");
     bt.print("\n");
-    
     delay(1000);//delay is related to the sampling rate
+    bt.print(String(sensorValue1) + " was sent from 1");
+    bt.print("\n");
+    delay(1000);
 }
