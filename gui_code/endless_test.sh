f08f8cefda3c81bcb2e5e6d070d0b0501cab58fc
mjhegarty <mhegarty@gwu.edu>
2019-04-28 18:25:00 -0400

    Worked on the gui a little bit more. Can start and stop bash process from GUI
    Worked on the tkinter gui a little bit more. Figured out that I will need to use python subprocesses using Popen.
    Eventually got a nifty solution to be able to kill a thread once it is created by having a class for the button that includes the button as well as
    the variable where the subprocess will be stored, then I can return that on the button push to allow the gui code to work like normal when the button is pushed,
    but still be able to kill the data stream when transmission is done. Might still be leaving an orphan becuase I'm not 100% sure if I am killing the process or just pausing it.
    I might need to wait for it after I kill it.
    
    In the future we will be able to have the bash script take standard output from the other python code and store it in a file via tee, then we can acess that file once we kill
    the subprocess to get the data collected during transmission.

---
+++ gui_code/endless_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while :
+    do
+    echo "Hello world from Bash"
+    done
+
--- gui_code/gui_prac.py
+++ gui_code/gui_prac.py
@@ -3,26 +3,33 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')
 from PIL import ImageTk, Image
-from subprocess import call
+import subprocess
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-def make_graph(x, canvas):
+def make_graph(x):
    fig = Figure(figsize=(6,6))
    a = fig.add_subplot(111)
    a.set_title ="test"
    a.plot(x, range(len(x)))
    return fig
 
-def show_graph(self):
-    if (self.graph["text"] == "show graph"):
-        print("got to show")
-        canvas.get_tk_widget().pack(side = "bottom")
-        self.graph["text"] = "hide graph"
-    else:
-        canvas.get_tk_widget().pack_forget()
-        print("got to forget")
-        self.graph["text"] = "show graph"
+
+class my_button():
+    def __init__(self, frame):
+        self.button = tk.Button(frame)
+        self.button["text"] = "start"
+        self.button["fg"] = "green"
+        self.button["command"] = lambda: self.get_data(frame)
+        self.p = None
+    def get_data(self, frame):
+        if self.p == None:
+            self.p = frame.get_data()  
+            print ('have p')
+        else :
+            print ('killing p')
+            frame.get_data(self.p)
+            self.p = None
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -32,14 +39,10 @@ class Application(tk.Frame):
         self.create_widgets()
 
 
-
-
     def create_widgets(self):
         both = "both"
-        self.start = tk.Button(self)
-        self.start["text"] = "start"
-        self.start["fg"] = "green"
-        self.start["command"] = self.say_hi
+        but = my_button(self)
+        self.start = but.button
         self.start.pack(side="top", fill=both, expand=1)
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
@@ -54,44 +57,48 @@ class Application(tk.Frame):
         
         
         fig1 = make_graph([1,2,3,4,5])
-        canvas = FigureCanvasTkAgg(fig1, master=self)
         
 
 
         self.graph = tk.Button(self)
         self.graph["text"] = "show graph"
         self.graph["fg"] = "blue"
-        self.graph["command"] = lambda: show_graph(self, canvas)
+        self.graph["command"] = lambda: self.show_graph(fig1)
         self.graph.pack(side="right", fill=both, expand=1)
-        #path = "testimage.png"
-        
-        
-
-        #self.img = ImageTk.PhotoImage(Image.open(path))
-
-        #self.panel = tk.Label(self, image=self.img)
-     #  # self.panel.image = self.img
-        #self.panel.pack(side = "bottom")
-
-
-        #fig1 = make_graph([1,2,3,4,5])
-        #fig2 = make_graph([1,2,3,4,5])
-        #canvas = FigureCanvasTkAgg(fig1, master=self)
-        #canvas.get_tk_widget().pack(side = "bottom")
-
-        #canvas2 = FigureCanvasTkAgg(fig2, master=self)
-        #canvas2.get_tk_widget().pack(side = "bottom")
 
     def pairing(self):
-        call("./blue_connect.sh")
+        subprocess.call("./blue_connect.sh")
+    def get_data(self, p=None): 
+        if (self.start["text"]=="start"):  
+            self.start["text"] = "stop"
+            self.start["fg"] = "red"
+            p = subprocess.Popen(("./endless_test.sh"))
+            return p
+        else:
+            self.start["text"]="start"
+            self.start["fg"]= "green"
+            p.kill()
+        
     def say_hi(self):
         if (self.start["text"]=="start"):  
             self.start["text"] = "stop"
             self.start["fg"] = "red"
-            call("./python_test.sh")
+            subprocess.call("./python_test.sh")
         else:
             self.start["text"]="start"
             self.start["fg"]= "green"
+
+    def show_graph(self, fig1):
+        print ("pushed button")
+        canvas = FigureCanvasTkAgg(fig1, master=self)
+        if (self.graph["text"] == "show graph"):
+            print("got to show")
+            canvas.get_tk_widget().pack(side = "bottom")
+            self.graph["text"] = "hide graph"
+        else:
+            canvas.get_tk_widget().pack_forget()
+            print("got to forget")
+            self.graph["text"] = "show graph"
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
